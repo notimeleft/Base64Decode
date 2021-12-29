c830f33bc4b7f5c796284e7de8bb02704c6a642f
Jerry Wang <jerry.wang.ct@gmail.com>
2021-12-29 08:12:46 -0700

    Initial Commit

---
+++ base64Decode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  base64Decode
+//
+//  Created by Jerry Wang on 12/29/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

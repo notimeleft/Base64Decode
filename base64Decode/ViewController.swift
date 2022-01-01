4781c134e8d16f6487246f30567def8444a6cae9
Jerry Wang <jerry.wang.ct@gmail.com>
2022-01-01 15:10:09 -0700

    added paste board functionality

--- base64Decode/ViewController.swift
+++ base64Decode/ViewController.swift
@@ -9,19 +9,29 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    @IBOutlet weak var base64TextField: NSTextField!
+
+    @IBOutlet weak var pasteButton: NSButton!
     @IBOutlet weak var linkButton: NSButton!
     var generatedLink: URL?
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        base64TextField.delegate = self
+        
+        if let pasteBoardContent = NSPasteboard.general.string(forType: .string) {
+            generatedLink = generateURL(from: pasteBoardContent)
+            let title = generatedLink?.absoluteString ?? "invalid link!"
+            pasteButton.title = "Decode"
+            linkButton.title = title
+        }
+        
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    @IBAction func didTapPasteBoard(_ sender: NSButton) {
+        if let pasteBoardContent = NSPasteboard.general.string(forType: .string) {
+            generatedLink = generateURL(from: pasteBoardContent)
+            let title = generatedLink?.absoluteString ?? "invalid link!"
+            linkButton.title = title
         }
     }
     
@@ -36,34 +46,29 @@ class ViewController: NSViewController {
     
 }
 
-extension ViewController: NSTextFieldDelegate {
-    func controlTextDidEndEditing(_ obj: Notification) {
-        if let validLink = base64TextField.stringValue.trimmingCharacters(in: .whitespacesAndNewlines).decodeRecursively() {
-            generatedLink = URL(string: validLink)
-            let title = generatedLink?.absoluteString ?? "invalid link!"
-            linkButton.title = title
-        }
+extension ViewController {
+    func generateURL(from input: String) -> URL? {
+        guard let validInput = input.decodeRecursively() else { return nil }
+        return URL(string:validInput)
     }
 }
 
 extension String {
     func decodeFromBase64() -> String? {
         guard !self.isEmpty,
-              let validData = Data(base64Encoded: self),
+              let validData = Data(base64Encoded: self.trimmingCharacters(in: .whitespacesAndNewlines)),
               let validString = String(data: validData, encoding: .utf8) else { return nil }
         return validString
     }
     
     func decodeRecursively() -> String? {
         if let validString = decodeFromBase64() {
-            print(validString)
             if validString.starts(with: "https://") {
                 return validString
             } else {
                 return validString.decodeRecursively()
             }
         } else {
-            print("returned nil")
             return nil
         }
     }

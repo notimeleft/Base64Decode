153ce1aca74f62385c55d17d0b4e104f5d1e6ba5
Jerry Wang <jerry.wang.ct@gmail.com>
2022-01-01 15:48:14 -0700

    refactored pasteboard call

--- base64Decode/ViewController.swift
+++ base64Decode/ViewController.swift
@@ -9,35 +9,29 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-
     @IBOutlet weak var pasteButton: NSButton!
     @IBOutlet weak var linkButton: NSButton!
-    var generatedLink: URL?
+    var url: URL?
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        if let pasteBoardContent = NSPasteboard.general.string(forType: .string) {
-            generatedLink = generateURL(from: pasteBoardContent)
-            let title = generatedLink?.absoluteString ?? "invalid link!"
-            pasteButton.title = "Decode"
-            linkButton.title = title
+        if let validLink = generateLink() {
+            pasteButton.title = "base64 decoded below:"
+            linkButton.title = validLink
+        } else {
+            linkButton.title = "..."
         }
-        
     }
     
     @IBAction func didTapPasteBoard(_ sender: NSButton) {
-        if let pasteBoardContent = NSPasteboard.general.string(forType: .string) {
-            generatedLink = generateURL(from: pasteBoardContent)
-            let title = generatedLink?.absoluteString ?? "invalid link!"
-            linkButton.title = title
-        }
+        linkButton.title = generateLink() ?? "invalid link!"
+        pasteButton.title = "decode"
     }
     
     
     @IBAction func didTapLink(_ sender: NSButton) {
-        if let validLink = generatedLink?.absoluteString {
+        if let validLink = url?.absoluteString {
             let executableURL = URL(fileURLWithPath: "/usr/bin/open")
             let args = ["-a", "Google Chrome", validLink]
             _ = try? Process.run(executableURL, arguments: args, terminationHandler: nil)
@@ -50,6 +44,13 @@ extension ViewController {
     func generateURL(from input: String) -> URL? {
         guard let validInput = input.decodeRecursively() else { return nil }
         return URL(string:validInput)
+    }
+    
+    func generateLink() -> String? {
+        if let pasteBoardContent = NSPasteboard.general.string(forType: .string) {
+            url = generateURL(from: pasteBoardContent)
+        }
+        return url?.absoluteString
     }
 }
 
